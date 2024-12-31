4d7787a6a09107c1461409d7f68f3ca9692c491d
AlexYang <m9939418@gmail.com>
2024-12-31 10:49:22 +0800

    Initial Commit

---
+++ AlexPokemonSwiftUI/AlexPokemonSwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+// AlexPokemonSwiftUIApp.swift
+// AlexPokemonSwiftUI
+//
+// Created by AlexYang on 2024/12/31.
+//
+
+
+import SwiftUI
+
+@main
+struct AlexPokemonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
